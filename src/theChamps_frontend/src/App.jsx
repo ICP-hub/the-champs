2af9e5ba3178fdf832067191d0c9d0448a5453a2
ankurnayak1 <ankur.nayak@quadbtech.com>
2024-04-06 12:01:05 +0530

    background ui

--- src/theChamps_frontend/src/App.jsx
+++ src/theChamps_frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { theChamps_backend } from "declarations/theChamps_backend";
 import HomePage from "./pages/HomePage";
 import MyProfilePage from "./pages/MyProfilePage";
@@ -10,6 +10,24 @@ import appRoutes from "./app.routing";
 
 function App() {
   const [greeting, setGreeting] = useState("");
+
+  // Set initial body background
+  document.body.style.background =
+    "linear-gradient(to left, rgba(38, 0, 252,0.1), rgba(255, 0, 234, 0.1))";
+
+  // Effect changing bg on scroll down
+  useEffect(() => {
+    const handleScroll = () => {
+      document.body.style.transition = "background 2s";
+      document.body.style.background =
+        window.scrollY > 800
+          ? "white"
+          : "linear-gradient(to left, rgba(38, 0, 252, 0.1), rgba(255, 0, 234, 0.1))";
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   function handleSubmit(event) {
     event.preventDefault();
