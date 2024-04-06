e4f668f843f6a202f3fcd08ca8bc26e2292730fb
ankurnayak1 <ankur.nayak@quadbtech.com>
2024-04-06 13:34:49 +0530

    bg-gradient-alpha-added

--- src/theChamps_frontend/src/App.jsx
+++ src/theChamps_frontend/src/App.jsx
@@ -12,16 +12,13 @@ function App() {
   const [greeting, setGreeting] = useState("");
 
   // Set initial body background
-  document.body.style.background =
-    "linear-gradient(90deg, hsl(15deg 80% 94%) 0%, hsl(7deg 100% 95%) 13%, hsl(355deg 100% 95%) 24%, hsl(340deg 100% 94%) 35%, hsl(324deg 100% 94%) 46%, hsl(306deg 94% 93%) 55%, hsl(275deg 100% 94%) 64%, hsl(246deg 100% 95%) 73%, hsl(222deg 100% 93%) 82%, hsl(211deg 100% 91%) 91%, hsl(203deg 88% 90%) 100%)";
+  document.body.className = "bg-champs-main";
   // Effect changing bg on scroll down
   useEffect(() => {
     const handleScroll = () => {
       document.body.style.transition = "background 2s";
-      document.body.style.background =
-        window.scrollY > 800
-          ? "white"
-          : "linear-gradient(90deg, hsl(15deg 80% 94%) 0%, hsl(7deg 100% 95%) 13%, hsl(355deg 100% 95%) 24%, hsl(340deg 100% 94%) 35%, hsl(324deg 100% 94%) 46%, hsl(306deg 94% 93%) 55%, hsl(275deg 100% 94%) 64%, hsl(246deg 100% 95%) 73%, hsl(222deg 100% 93%) 82%, hsl(211deg 100% 91%) 91%, hsl(203deg 88% 90%) 100%)";
+      document.body.className =
+        window.scrollY > 800 ? "bg-champs-white" : "bg-champs-main";
     };
 
     window.addEventListener("scroll", handleScroll);
