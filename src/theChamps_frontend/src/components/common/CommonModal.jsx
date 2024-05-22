4490a2969bc7c71a7a3b5492ddf375d1be4fc303
riteshQB <ritesh.kumar@quadbtech.com>
2024-05-22 11:19:55 +0530

    addeD

--- src/theChamps_frontend/src/components/common/CommonModal.jsx
+++ src/theChamps_frontend/src/components/common/CommonModal.jsx
@@ -1,9 +1,48 @@
-import { motion, AnimatePresence } from "framer-motion";
+import { useCanister, useConnect } from "@connect2ic/react";
+import { Principal } from "@dfinity/principal";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import toast from "react-hot-toast";
 import { RiErrorWarningFill } from "react-icons/ri";
 
-const CommonModal = ({ toggleModal }) => {
+const CommonModal = ({ toggleModal, tokencanisterid }) => {
+  const { isConnected, principal } = useConnect();
+  const [backend] = useCanister("backend");
+  const [receiver, setReceiver] = useState("");
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setReceiver(e.target.value);
+  };
+
+  const handleTransferNow = async () => {
+    setLoading(true);
+    if (!isConnected) {
+      try {
+        const response = await backend.tranfertokens(
+          tokencanisterid,
+          Principal.fromText(receiver),
+          BigInt(0)
+        );
+
+        if (response.Ok) {
+          toast.log("Transfer successful");
+        } else {
+          toast.error("Transfer error:", response.Err);
+          console.error("Transfer error:", response.Err);
+        }
+      } catch (error) {
+        console.error("Error occurred during transfer:", error);
+        toast.error("Error occurred during transfer");
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      toast.error("Please login first");
+    }
+  };
+
   return (
-    <div className="h-screen w-screen top-0 bottom-0 left-0 right-0 flex items-center justify-center bg-[rgba(0,0,0,0.5)]">
+    <div className="h-screen w-screen top-0 bottom-0 left-0 right-0 flex items-center justify-center bg-transparent">
       <motion.div
         initial={{ scale: 0, opacity: 0 }}
         animate={{
@@ -25,10 +64,11 @@ const CommonModal = ({ toggleModal }) => {
             Beware, not all wallets support all token
           </p>
           <span className="flex flex-col">
-            <h4 className="text-gray=400 py-4">
+            <h4 className="text-gray-400 py-4">
               Address or Principal of receiver
             </h4>
             <input
+              onChange={handleChange}
               type="text"
               className="outline-none focus:outline-none border-b-2 border-gray-400"
             />
@@ -37,7 +77,12 @@ const CommonModal = ({ toggleModal }) => {
             <button className="px-4 py-2 rounded-md" onClick={toggleModal}>
               Back
             </button>
-            <button className="button px-4 py-2 rounded-md text-white">
+            <button
+              className={`button px-4 py-2 rounded-md text-white ${
+                loading ? "opacity-50" : ""
+              }`}
+              onClick={handleTransferNow}
+            >
               Transfer
             </button>
           </div>
--- src/theChamps_frontend/src/components/myProfile/MyFav.jsx
+++ src/theChamps_frontend/src/components/myProfile/MyFav.jsx
@@ -10,6 +10,7 @@ import { Principal } from "@dfinity/principal";
 import {
   Bars,
   CirclesWithBar,
+  InfinitySpin,
   RotatingLines,
   TailSpin,
 } from "react-loader-spinner";
@@ -24,53 +25,40 @@ const MyFav = () => {
   const [backend] = useCanister("backend");
 
   const [favourites, setFavourites] = useState([]);
-  const [productInFavourites, setProductInFavourites] = useState(false);
   const [loading, setLoading] = useState(false);
   const [loading2, setLoading2] = useState(true);
   const [showModal, setShowModal] = useState(false);
   const [assets] = useBalance();
-  const [product, setProduct] = useState([]);
+  const [filteredProduct, setFilteredProduct] = useState([]);
+  const rincipal = Principal.fromText("2vxsx-fae");
 
   useEffect(() => {
     const getUsersFractionNFT = async () => {
       try {
-        const canister_id = Principal.fromText("2vxsx-fae");
-        const res = await backend.getusersfractionnft(canister_id);
-        setProduct(res);
+        const res = await backend.getusersfractionnft(rincipal);
+        const userProducts = res.map((item) => item[1]);
+        const favouritesRes = await backend.getfavourites();
+
+        const favouriteProducts = userProducts.filter((product) =>
+          favouritesRes.some((fav) => fav[0].id === product.nft.id)
+        );
+
+        setFilteredProduct(favouriteProducts);
         setLoading2(false);
-        console.log("Favorite", res);
+        console.log("Favourite Products", favouriteProducts);
       } catch (error) {
         console.log("Error while fetching user NFT", error);
       }
     };
 
-    const getFavourites = async () => {
-      try {
-        const res = await backend.getfavourites();
-        setFavourites(res);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
     getUsersFractionNFT();
-    getFavourites();
   }, [backend, principal]);
 
-  useEffect(() => {
-    if (favourites.length > 0 && product.length > 0) {
-      const isProductInWishlist = favourites.some(
-        (item) => item[0].id === product[0]?.nft?.id && item[1].toText() === id
-      );
-      setProductInFavourites(isProductInWishlist);
-    }
-  }, [product, favourites, id]);
-
-  const addToFavourites = async () => {
+  const addToFavourites = async (productId) => {
     try {
       setLoading(true);
       const canister_id = Principal.fromText(id);
-      await backend.addfavourite(canister_id, parseInt(product[0]?.nft?.id));
+      await backend.addfavourite(canister_id, productId);
       toast.success("Item added to favourites");
       // Refresh favourites
       const res = await backend.getfavourites();
@@ -83,11 +71,11 @@ const MyFav = () => {
     }
   };
 
-  const removeFavourites = async () => {
+  const removeFavourites = async (productId) => {
     try {
       setLoading(true);
       const canister_id = Principal.fromText(id);
-      await backend.removefavourite(canister_id, parseInt(product[0]?.nft?.id));
+      await backend.removefavourite(canister_id, productId);
       toast.success("Item removed from favourites");
       // Refresh favourites
       const res = await backend.getfavourites();
@@ -117,127 +105,135 @@ const MyFav = () => {
     <div>
       {loading2 ? (
         <div className="flex items-center h-56 justify-center">
-          <Bars
-            height="120"
-            width="120"
-            color="#FC001E"
-            ariaLabel="bars-loading"
-            wrapperStyle={{}}
-            wrapperClass=""
+          <InfinitySpin
             visible={true}
+            width="200"
+            color="#FC001E"
+            ariaLabel="infinity-spin-loading"
           />
         </div>
-      ) : (
+      ) : filteredProduct.length > 0 ? (
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-          {product.map((product, index) => (
-            <div className="">
-              {" "}
-              <div
-                key={index}
-                className="border rounded-xl overflow-hidden"
-                style={{ boxShadow: "4px 4px 10px rgba(0, 0, 0, 0.2)" }}
-              >
-                <div className="overflow-hidden">
-                  <Link to={`/collections/${id}/${product[0]?.nft?.id}`}>
-                    <motion.img
-                      whileHover={{ scale: 1.1 }}
-                      transition={{ duration: 0.2, ease: "easeInOut" }}
-                      src={product[0]?.fractional_token?.logo || placeHolderImg}
-                      alt=""
-                      className="rounded-t-lg h-full object-cover cursor-pointer overflow-hidden"
+          {filteredProduct.map((product, index) => (
+            <div
+              key={index}
+              className="border rounded-xl overflow-hidden"
+              style={{ boxShadow: "4px 4px 10px rgba(0, 0, 0, 0.2)" }}
+            >
+              <div className="overflow-hidden">
+                <Link
+                  to={`/collections/${product?._Principal?.toText()}}/${
+                    product.nft.id
+                  }`}
+                >
+                  <motion.img
+                    whileHover={{ scale: 1.1 }}
+                    transition={{ duration: 0.2, ease: "easeInOut" }}
+                    src={placeHolderImg || product.fractional_token.logo}
+                    alt=""
+                    className="rounded-t-lg h-full object-cover cursor-pointer overflow-hidden"
+                  />
+                </Link>
+              </div>
+              <div className="p-2 mx-2">
+                <div className="flex justify-between font-bold items-center">
+                  <h2 className="text-lg font-semibold mb-2">
+                    {product.fractional_token.name}
+                  </h2>
+                  {loading ? (
+                    <TailSpin
+                      height="8%"
+                      width="8%"
+                      color="black"
+                      ariaLabel="tail-spin-loading"
+                      radius="1"
+                      visible={true}
                     />
-                  </Link>
+                  ) : (
+                    <>
+                      {favourites.some(
+                        (fav) => fav[0].id === product.nft.id
+                      ) ? (
+                        <button
+                          onClick={() => removeFavourites(product.nft.id)}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 115.77 122.88"
+                            width="30"
+                            height="25"
+                            className="gradient-icon"
+                          >
+                            <defs>
+                              <linearGradient
+                                id="gradient"
+                                x1="0"
+                                y1="0"
+                                x2="100%"
+                                y2="0"
+                              >
+                                <stop offset="0%" stopColor="#FC001E" />
+                                <stop offset="100%" stopColor="#FF7D57" />
+                              </linearGradient>
+                            </defs>
+                            <path
+                              fill="url(#gradient)"
+                              d="M60.83,17.19C68.84,8.84,74.45,1.62,86.79,0.21c23.17-2.66,44.48,21.06,32.78,44.41 c-3.33,6.65-10.11,14.56-17.61,22.32c-8.23,8.52-17.34,16.87-23.72,23.2l-17.4,17.26L46.46,93.56C29.16,76.9,0.95,55.93,0.02,29.95 C-0.63,11.75,13.73,0.09,30.25,0.3C45.01,0.5,51.22,7.84,60.83,17.19L60.83,17.19L60.83,17.19z"
+                            />
+                          </svg>
+                        </button>
+                      ) : (
+                        <button onClick={() => addToFavourites(product.nft.id)}>
+                          <CiHeart size={32} />
+                        </button>
+                      )}
+                    </>
+                  )}
                 </div>
-                <div className="p-2 mx-2">
-                  <div className="flex justify-between font-bold items-center">
-                    <h2 className="text-lg font-semibold mb-2">
-                      {product[0]?.fractional_token?.name}
-                    </h2>
-                    {loading ? (
-                      <TailSpin
-                        height="8%"
-                        width="8%"
-                        color="black"
-                        ariaLabel="tail-spin-loading"
-                        radius="1"
-                        visible={true}
-                      />
-                    ) : (
-                      <>
-                        {productInFavourites ? (
-                          <button onClick={removeFavourites}>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 115.77 122.88"
-                              width="30"
-                              height="25"
-                              className="gradient-icon"
-                            >
-                              <defs>
-                                <linearGradient
-                                  id="gradient"
-                                  x1="0"
-                                  y1="0"
-                                  x2="100%"
-                                  y2="0"
-                                >
-                                  <stop offset="0%" stopColor="#FC001E" />
-                                  <stop offset="100%" stopColor="#FF7D57" />
-                                </linearGradient>
-                              </defs>
-                              <path
-                                fill="url(#gradient)"
-                                d="M60.83,17.19C68.84,8.84,74.45,1.62,86.79,0.21c23.17-2.66,44.48,21.06,32.78,44.41 c-3.33,6.65-10.11,14.56-17.61,22.32c-8.23,8.52-17.34,16.87-23.72,23.2l-17.4,17.26L46.46,93.56C29.16,76.9,0.95,55.93,0.02,29.95 C-0.63,11.75,13.73,0.09,30.25,0.3C45.01,0.5,51.22,7.84,60.83,17.19L60.83,17.19L60.83,17.19z"
-                              />
-                            </svg>
-                          </button>
-                        ) : (
-                          <button onClick={addToFavourites}>
-                            <CiHeart size={32} />
-                          </button>
-                        )}
-                      </>
-                    )}
-                  </div>
-                  <p className="text-gray-500 text-sm">
-                    <ReadMore
-                      text={product[0]?.fractional_token?.owner?.toText() || ""}
-                      maxLength={20}
-                    />
+                <p className="text-gray-500 text-sm">
+                  <ReadMore
+                    text={product.fractional_token.owner.toText() || ""}
+                    maxLength={20}
+                  />
+                </p>
+                <div className="flex justify-between mb-4">
+                  <p className="mt-4 py-2 rounded-md w-[50%]">
+                    {parseInt(product.fractional_token.fee) || 0}
                   </p>
-                  <div className="flex justify-between mb-4">
-                    <p className="mt-4 py-2 rounded-md w-[50%]">
-                      {parseInt(product[0]?.fractional_token?.fee) || 0}
-                    </p>
+                  <button
+                    className="mt-4 button bg-opacity-100 text-white rounded-md w-[50%] text-md flex items-center justify-center"
+                    onClick={handleBuyNow}
+                  >
+                    Buy now
+                  </button>
+                </div>
+              </div>
+
+              {/* Modal for insufficient balance */}
+              {showModal && (
+                <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75">
+                  <div className="bg-white p-4 rounded-lg flex flex-col space-x-5 space-y-8 items-center justify-center">
+                    <IconWrapper>
+                      <RiErrorWarningLine size={36} />
+                    </IconWrapper>
+                    <p>You don't have sufficient balance to buy this NFT.</p>
                     <button
-                      className="mt-4 button bg-opacity-100 text-white rounded-md w-[50%] text-md flex items-center justify-center"
-                      onClick={handleBuyNow}
+                      className="mt-2 px-4 py-2 button bg-blue-500 text-white rounded-lg"
+                      onClick={() => setShowModal(false)}
                     >
-                      Buy now
+                      Close
                     </button>
                   </div>
                 </div>
-
-                {/* Modal for insufficient balance */}
-                {showModal && (
-                  <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75">
-                    <div className="bg-white p-4 rounded-lg flex flex-col space-x-5 space-y-8 items-center justify-center">
-                      <IconWrapper>
-                        <RiErrorWarningLine size={36} />
-                      </IconWrapper>
-                      <p>You don't have sufficient balance to buy this NFT.</p>
-                      <button
-                        className="mt-2 px-4 py-2 button bg-blue-500 text-white rounded-lg"
-                        onClick={() => setShowModal(false)}
-                      >
-                        Close
-                      </button>
-                    </div>
-                  </div>
-                )}
-              </div>
+              )}
             </div>
           ))}
+        </div>
+      ) : (
+        <div className="text-center mt-8 px-6 lg:px-24  flex justify-center items-center">
+          <button className="px-4 py-2  cursor-pointer rounded-lg w-48 productcardlgborder z-[1]">
+            No NFT found
+          </button>
         </div>
       )}
     </div>
